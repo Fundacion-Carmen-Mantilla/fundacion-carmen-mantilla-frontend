af92f4d099f968b8e3ef931ed907f1e79436c1a6
Jgonzalez <jesusjoelgonzalezcastro@gmail.com>
2025-02-02 18:33:48 -0500

    Refactor navbar component for improved structure and styles
    Replaced dynamic menu fetching with static definitions for better control and structured them into categories like "Ministerio de Cultura" and "Gobernación del Magdalena." Updated HTML for a more responsive and user-friendly design, integrating mega menus and accordions. Applied consistent CSS styling with better hover effects and animations for a clean and modern UI.

--- src/app/shared/features/components/navbar/navbar.component.ts
+++ src/app/shared/features/components/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import {NavItem} from '../../../../core/models/navigation.interface';
 import {HttpClient} from '@angular/common/http';
 import {IResponseSuccess} from '../../../../core/models/responses.interface';
 
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 import {NgClass, NgTemplateOutlet} from '@angular/common';
 
 @Component({
@@ -12,7 +12,7 @@ import {NgClass, NgTemplateOutlet} from '@angular/common';
   imports: [
     RouterLink,
     NgClass,
-    NgTemplateOutlet
+    RouterLinkActive,
   ],
   providers: [NavigationService, HttpClient],
   templateUrl: './navbar.component.html',
@@ -20,108 +20,241 @@ import {NgClass, NgTemplateOutlet} from '@angular/common';
 })
 export class NavbarComponent implements OnInit {
 
-  menuItems: NavItem[] = [];
+  //menuItems: NavItem[] = [];
   IsLogged: any;
   isMobile: boolean = false;
   navigationService = inject(NavigationService);
+  menuItems: NavItem[] = [
+    {
+      "label": "Home",
+      "url": "/home",
+      "items": []
+    },
+    {
+      "label": "u00bfQuiu00e9nes Somos?",
+      "url": "/quienes-somos",
+      "items": []
+    },
+    {
+      "label": "Proyectos Culturales",
+      "url": "#",
+      "items": [
+        {
+          "label": "Danza por Parejas",
+          "url": "#",
+          "items": [
+            {
+              "label": "Informacion",
+              "url": "/proyectos-culturales/danza-por-parejas/informacion",
+              "items": []
+            },
+            {
+              "label": "Galeria",
+              "url": "/proyectos-culturales/danza-por-parejas/galeria",
+              "items": []
+            }
+          ]
+        },
+        {
+          "label": "Gobernaciu00f3n del Magdalena",
+          "url": "#",
+          "items": [
+            {
+              "label": "2020",
+              "url": "#",
+              "items": [
+                {
+                  "label": "Formar para Danzar en el Pau00eds del Pocabuy",
+                  "url": "/proyectos-culturales/gobernacion-magdalena/2022/formar-para-danzar",
+                  "items": []
+                },
+                {
+                  "label": "Cartilla Virtual",
+                  "url": "https://www.calameo.com/read/006464616e1d236399058",
+                  "target": "_blank",
+                  "items": []
+                }
+              ]
+            },
+            {
+              "label": "2023",
+              "url": "#",
+              "items": [
+                {
+                  "label": "Talleres de Danza Conservemos la Tradiciu00f3n y el Patrimonio",
+                  "url": "/proyectos-culturales/gobernacion-magdalena/2023/talleres-de-danza",
+                  "items": []
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "label": "Galeria",
+      "url": "/galeria",
+      "items": []
+    },
+    {
+      "label": "Academia",
+      "url": "/academia",
+      "items": []
+    },
+    {
+      "label": "Ayuda Espiritual",
+      "url": "#",
+      "items": [
+        {
+          "label": "Reflexiones",
+          "url": "/reflexiones",
+          "items": []
+        },
+        {
+          "label": "Oraciones",
+          "url": "/oraciones",
+          "items": []
+        },
+        {
+          "label": "Santo Rosario",
+          "url": "/santo-rosario",
+          "items": []
+        }
+      ]
+    },
+    {
+      "label": "Donaciu00f3n",
+      "url": "/donacion",
+      "items": []
+    },
+    {
+      "label": "Contacto",
+      "url": "/contacto",
+      "items": []
+    }
+  ];
 
-  constructor(
-  ) {
+  ministerioCulture: NavItem[] = [
+    {
+      "label": "Ministerio de Cultura",
+      "url": "#",
+      "items": [
+        {
+          "label": "2020",
+          "url": "#",
+          "items": [
+            {
+              "label": "#ComparteLoQueSomos",
+              "url": "/proyectos-culturales/min-cultura/2020/comparte-lo-que-somos",
+            }
+          ]
+        },
+        {
+          "label": "2021",
+          "url": "#",
+          "items": [
+            {
+              "label": "Aprendiendo y Danzando",
+              "url": "/proyectos-culturales/min-cultura/2021/aprendiendo-y-danzando",
+              "items": []
+            },
+            {
+              "label": "Formados Danzamos Unidos",
+              "url": "/proyectos-culturales/min-cultura/2021/formados-danzamos-unidos",
+              "items": []
+            }
+          ]
+        },
+        {
+          "label": "2022",
+          "url": "#",
+          "items": [
+            {
+              "label": "Aprendiendo, Danzando y Formados Seguimos Avanzando",
+              "url": "/proyectos-culturales/min-cultura/2022/aprendiendo-danzando-y-formados",
+              "items": []
+            }
+          ]
+        },
+        {
+          "label": "2023",
+          "url": "#",
+          "items": [
+            {
+              "label": "Aprendiendo, Danzamos por la Vida al Ritmo de la Paz",
+              "url": "/proyectos-culturales/min-cultura/2023/aprendiendo-danzamos-vida",
+              "items": []
+            }
+          ]
+        },
+        {
+          "label": "2024",
+          "url": "#",
+          "items": [
+            {
+              "label": "Aprendiendo, Conservamos el Patrimonio Cultural y Medioambiental",
+              "url": "/proyectos-culturales/min-cultura/2024/aprendiendo-patrimonio-medioambiental",
+              "items": []
+            }
+          ]
+        }
+      ]
+    },
+  ];
+
+  gobernacionMagdalena: NavItem[] = [
+    {
+      "label": "Gobernacion del Magdalena",
+      "url": "#",
+      "items": [
+        {
+          "label": "2020",
+          "url": "#",
+          "items": [
+            {
+              "label": "Formar para Danzar en el Pais del Pocabuy",
+              "url": "/proyectos-culturales/gobernacion-magdalena/2022/formar-para-danzar",
+              "items": []
+            }
+          ]
+        },
+        {
+          "label": "2023",
+          "url": "#",
+          "items": [
+            {
+              "label": "Talleres de Danza Conservemos la Tradicion y el Patrimonio",
+              "url": "/proyectos-culturales/gobernacion-magdalena/2023/talleres-de-danza",
+              "items": []
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  danzaParejas: NavItem[] = [
+    {
+      "label": "Danza por Parejas",
+      "url": "#",
+      "items": [
+        {
+          "label": "Informacion",
+          "url": "/proyectos-culturales/danza-por-parejas/informacion",
+          "items": []
+        },
+        {
+          "label": "Galeria",
+          "url": "/proyectos-culturales/danza-por-parejas/galeria",
+          "items": []
+        }
+      ]
+    }];
+
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.navigationService.getMenu().subscribe({
-      next: (response: IResponseSuccess<NavItem[]>) => {
-        // Asignar los datos del menú a la variable del componente
-        this.menuItems = response.data || []; // Usamos el primer nivel del array 'data[0]'
-      },
-      error: (error) => {
-        console.error('Error al recibir el menú', error);
-        this.menuItems = [];
-      }
-    });
-    this.checkScreenSize();
-  }
 
-  @HostListener('window:resize')
-  onResize() {
-    this.checkScreenSize();
-  }
-
-  private checkScreenSize() {
-    if (typeof window !== 'undefined') { // Verifica si 'window' está disponible
-      this.isMobile = window.innerWidth <= 768;
-      if (this.isMobile) {
-        this.closeAllDropdowns();
-      }
-    }
-  }
-
-  toggleDropdown(event: Event, level: number = 0): void {
-    event.preventDefault();
-    event.stopPropagation();
-
-    const target = event.target as HTMLElement;
-    const parentLi = target.closest('li');
-
-    if (!parentLi) return;
-
-    // Remover cualquier estado de hover previo
-    this.clearHoverStates();
-
-    const dropdown = parentLi.querySelector('.dropdown-menu') as HTMLElement;
-
-    if (dropdown) {
-      // Si estamos en móvil, manejamos diferente el comportamiento
-      if (this.isMobile) {
-        dropdown.classList.toggle('show');
-        target.classList.toggle('active');
-      } else {
-        // En desktop, cerramos los submenús del mismo nivel
-        const currentLevel = parentLi.closest(`ul`);
-        if (currentLevel) {
-          const siblingDropdowns = currentLevel.querySelectorAll('.dropdown-menu.show');
-          siblingDropdowns.forEach(menu => {
-            if (menu !== dropdown) {
-              menu.classList.remove('show');
-              const parentToggle = menu.parentElement?.querySelector('.dropdown-toggle');
-              if (parentToggle) {
-                parentToggle.classList.remove('active');
-              }
-            }
-          });
-        }
-
-        dropdown.classList.toggle('show');
-        target.classList.toggle('active');
-      }
-    }
-  }
-
-  private clearHoverStates(): void {
-    // Remover estados de hover previos
-    document.querySelectorAll('.hover-active').forEach(element => {
-      element.classList.remove('hover-active');
-    });
-  }
-
-  private closeAllDropdowns(): void {
-    document.querySelectorAll('.dropdown-menu.show').forEach(dropdown => {
-      dropdown.classList.remove('show');
-    });
-
-    document.querySelectorAll('.dropdown-toggle.active').forEach(toggle => {
-      toggle.classList.remove('active');
-    });
-  }
-
-  // Método para cerrar al hacer clic fuera
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: Event): void {
-    const target = event.target as HTMLElement;
-    if (!target.closest('.dropdown')) {
-      this.closeAllDropdowns();
-    }
   }
 
 }
