af0c3ab58e4ba8c73a9e997818f730f30302f1ee
Jgonzalez <jesusjoelgonzalezcastro@gmail.com>
2025-02-09 22:11:24 -0500

    feat(home): add models for Noticia and Post

---
+++ src/app/features/home/models/home.model.ts
@@ -0,0 +1,16 @@
+export interface Noticia {
+  id: number;
+  titulo: string;
+  resumen: string;
+  imagen: string;
+  fecha: Date;
+}
+
+export interface Post {
+  id: number;
+  titulo: string;
+  resumen: string;
+  imagen: string;
+  autor: string;
+  fecha: Date;
+}
